5d852902fdc5a49e97e3de22c42f4b1a36ab56ec
Rodolfo Talan <rtalan@gmail.com>
2020-06-23 13:40:31 -0600

    list_display.

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 import datetime
+from django.utils import timezone
 
 from django.db import models
-from django.utils import timezone
 
 
 class Question(models.Model):
@@ -11,7 +11,10 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now           
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
